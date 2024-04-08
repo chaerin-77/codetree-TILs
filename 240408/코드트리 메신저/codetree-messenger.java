27649a760be7174c37078b035c3cc321d1391150
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-04-08 20:14:01 +0900

    Title: 코드트리 메신저, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240408/코드트리 메신저/codetree-messenger.java
+++ 240408/코드트리 메신저/codetree-messenger.java
@@ -108,8 +108,8 @@ public class Main {
 
     private static void checkNotify(int num) {
         Deque<Node> q = new ArrayDeque<>();
-        q.offer(chat[chat[num].child1]);
-        q.offer(chat[chat[num].child2]);
+        if (chat[num].child1 != -1) q.offer(chat[chat[num].child1]);
+        if (chat[num].child1 != -1)q.offer(chat[chat[num].child2]);
 
         int depth = 1;
         while (!q.isEmpty()) {
